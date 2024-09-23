4666c2c31d17f95df035a12f32bfb8bea8910a52
ColonialKreeper <bennettsmith2026@gmail.com>
2024-09-23 13:01:01 -0400

    modified:   package.json 	modified:   src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.insertComments', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const firstLine = "Bennett Smith";
+            const date = new Date();
+            const formattedDate = `${String(date.getDate()).padStart(2, '0')}.${String(date.getMonth() + 1).padStart(2, '0')}.${date.getFullYear()}`;
+			const fileNameWithExtension = editor.document.fileName.split('/').pop();
+			const fileName = fileNameWithExtension ? path.parse(fileNameWithExtension).name : '';
+			
+			const thirdLine = `test ${fileName}`;
+			
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "comment-inserter" is now active!');
+            const comment = `// ${firstLine}\n// ${formattedDate}\n// ${thirdLine}\n`;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('comment-inserter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Comment Inserter!');
-	});
+            editor.edit(editBuilder => {
+                editBuilder.insert(new vscode.Position(0, 0), comment);
+            });
+        }
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
+
+    vscode.workspace.onDidCreateFiles(event => {
+        event.files.forEach(file => {
+            if (file.fsPath.endsWith('.java')) {
+                vscode.commands.executeCommand('extension.insertComments');
+            }
+        });
+    });
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
